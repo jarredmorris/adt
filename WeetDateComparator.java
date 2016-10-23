5f10c5dd68f61452067bbe62ebc59f1ab0ec6c13
Jarred Morris <jarredmorris@users.noreply.github.com>
2016-10-23 18:25:42 +0100

    Extension of previous commit

--- WeetDateComparator.java
+++ WeetDateComparator.java
@@ -1,15 +1,15 @@
 /**
  * @author Jarred Morris
- * A Comparator to impose a total order on Weet objects according to their date.
+ * A Comparator to impose a total order on Tweet objects according to their date.
  **/
 public class TweetDateComparator implements Comparator<Tweet> {
 
 	/**
 	 * inheritDoc
-	 * Orders the Weet objects by their Date posted
+	 * Orders the Tweet objects by their Date posted
 	 */
 	public int compare(Tweet e1, Tweet e2) {
-		// Compares the dates the weets were posted, and return this value
+		// Compares the dates the tweets were posted, and return this value
 		return e1.getDate().compareTo(e2.getDate());
 	}
 }
