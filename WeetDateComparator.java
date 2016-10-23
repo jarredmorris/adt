1dd5adf87ee625e2dde4ceb73e5bbac1da6262b1
Jarred Morris <jarredmorris@users.noreply.github.com>
2016-10-23 18:25:05 +0100

    Plagiarism prevention changes
    Tweets on this coursework have a very specific name; to prevent indexing by search engines, I have adjusted this on this commit. Please see the README.
    
    Note that previously, a basic implementation was provided for the Tweet class (under its older, identifiable name). This was purely to allow the comparator implementation to compile without providing access to the proprietary class. This has now been removed as it delivered undefined behaviour and was there merely to ensure compilation. In the actual use, that class operated very differently.

--- WeetDateComparator.java
+++ WeetDateComparator.java
@@ -2,20 +2,14 @@
  * @author Jarred Morris
  * A Comparator to impose a total order on Weet objects according to their date.
  **/
-public class WeetDateComparator implements Comparator<Weet> {
+public class TweetDateComparator implements Comparator<Tweet> {
 
 	/**
 	 * inheritDoc
 	 * Orders the Weet objects by their Date posted
 	 */
-	public int compare(Weet e1, Weet e2) {
+	public int compare(Tweet e1, Tweet e2) {
 		// Compares the dates the weets were posted, and return this value
 		return e1.getDate().compareTo(e2.getDate());
-	}
-}
-
-class Weet {
-	public Integer getDate() {
-		return 0;
 	}
 }
